a7fc5c77fc0e03a2d318ba3b42580adbc4dbbc6c
sharklatan <element.ro@gmail.com>
2017-05-28 20:15:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
+<center>
+<h1>
+JailBreakMe Bot
+</h1>
+© 2017 Shark-Design 
+</center>
+<br><br><br>
+
+
+<?php
+
+ini_set('date.timezone', 'Asia/Calcutta');
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+define('ROOT', __DIR__.DIRECTORY_SEPARATOR);
+$ROOT = __DIR__;
+ini_set('error_log', ROOT.'error_log');
+
+
+require('bot.php');
+
+$bot = new bot('373936379:AAGbYbnKHWzVLLoTOtBdkphB_a6GeROHHjE');
+$bot -> uploadfile = true;
+$bot -> chatid = 15310317;
+
+
+// load json content
+$content = file_get_contents('content.json');
+$new_hash = sha1($content.json);
+
+
+// check if an update is available
+
+$old_hash = is_file('json_hash') ? file_get_contents('json_hash') : '';
+if($old_hash === $new_hash)
+{
+  return false;
+}
+
+
+
+// make response body
+$content = json_decode($content, true);
+$str = "<b>Updates</b>\n\n";
+$str .= "";
+foreach($content -> iPhone7 -> firmwares as $file)
+{
+  $str .= " {$file -> version} ". ($file -> signing ? '✅' : '❌') . " {$file -> stopped}\n\n";
+}
+// send message to user/channel
+$bot -> sendMessage($str, 'HTML');
+// write new hash
+file_put_contents('json_hash', $new_hash)
+?>
+
 
